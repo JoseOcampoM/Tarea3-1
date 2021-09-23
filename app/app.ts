13221693cd9c7e18fd6bffcce6aa255b9d6bd003
SandraDaza <sandrapdaza@uniguajira.edu.co>
2021-09-22 23:55:32 -0500

    Actualizacion 6

--- app/app.ts
+++ app/app.ts
@@ -1,29 +1,27 @@
-//array
 
-let modelo: string[] = ["Beat","Onix turbo sedan", "joy","Joy sedan","onix turbo rs","onix turbo hb",
-"Colorado","camaro six ss",];
-console.log(modelo);
-
-let cantautos:number[] = [20, 7, 12, 5, 10, 18, 27, 15];
-console.log(cantautos);
 
 //Tuplas
 
+let cliente1:[string, string, string, number]= ["jose", "Colorado", "chevrolet",312456778]
+cliente1.forEach(element => {
+    console.log(element)
+});
+let cliente2:[string, string, string, number]= ["Sandra", "camaro six ss", "chevrolet",3134727890]
+cliente2.forEach(element => {
+    console.log(element)
+});
+
+let cliente3:[string, string, string, number]= ["Dubian", "Joy", "chevrolet",3103937621]
+cliente3.forEach(element => {
+    console.log(element)
+});
 
 //objetos
 
-
-//funcion
-
-function calcularDeudaconparametros(Pago:number, Precio:number):number{
-    Pago=20000000;
-    Precio=40000290;
-  const deuda= Precio-Pago;
-  return deuda
-  }
-  
-  
-  let Pago:number=20000000;
-  let Precio:number=40000290;
-  const deuda= calcularDeudaconparametros(Precio,Pago)
-  console.log(`la deuda es de ${deuda}`);
+interface clientes {
+    cedula:number
+    Nombre: String
+    Apellido: string
+    Telefono:number
+    Ciudad:string
+}
--- js/app.js
+++ js/app.js
@@ -1,17 +1,13 @@
-"use strict";
-let modelo = ["Beat", "Onix turbo sedan", "joy", "Joy sedan", "onix turbo rs", "onix turbo hb",
-    "Colorado", "camaro six ss",];
-console.log(modelo);
-let cantautos = [20, 7, 12, 5, 10, 18, 27, 15];
-console.log(cantautos);
 
-function calcularDeudaconparametros(Pago, Precio) {
-    Pago = 20000000;
-    Precio = 40000290;
-    const deuda = Precio - Pago;
-    return deuda;
-}
-let Pago = 20000000;
-let Precio = 40000290;
-const deuda = calcularDeudaconparametros(Precio, Pago);
-console.log(`la deuda es de ${deuda}`);
+let cliente1 = ["jose", "Colorado", "chevrolet", 312456778];
+cliente1.forEach(element => {
+    console.log(element);
+});
+let cliente2 = ["Sandra", "camaro six ss", "chevrolet", 3134727890];
+cliente2.forEach(element => {
+    console.log(element);
+});
+let cliente3 = ["Dubian", "Joy", "chevrolet", 3103937621];
+cliente3.forEach(element => {
+    console.log(element);
+});
