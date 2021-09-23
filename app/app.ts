8d0e91a98ac01e2c61474a6b26c9075e74695c45
JoseOcampo <jocampo@uniguajira.edu.co>
2021-09-23 00:02:05 -0500

    Actualizacion 7

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,11 @@
+//array
 
+let modelo: string[] = ["Beat","Onix turbo sedan", "joy","Joy sedan","onix turbo rs","onix turbo hb",
+"Colorado","camaro six ss",];
+console.log(modelo);
+
+let cantautos:number[] = [20, 7, 12, 5, 10, 18, 27, 15];
+console.log(cantautos);
 
 //Tuplas
 
@@ -25,3 +32,19 @@ interface clientes {
     Telefono:number
     Ciudad:string
 }
+
+
+//funcion
+
+function calcularDeudaconparametros(Pago:number, Precio:number):number{
+    Pago=20000000;
+    Precio=40000290;
+  const deuda= Precio-Pago;
+  return deuda
+  }
+  
+  
+  let Pago:number=20000000;
+  let Precio:number=40000290;
+  const deuda= calcularDeudaconparametros(Precio,Pago)
+  console.log(`la deuda es de ${deuda}`);
--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-
+"use strict";
+let modelo = ["Beat", "Onix turbo sedan", "joy", "Joy sedan", "onix turbo rs", "onix turbo hb",
+    "Colorado", "camaro six ss",];
+console.log(modelo);
+let cantautos = [20, 7, 12, 5, 10, 18, 27, 15];
+console.log(cantautos);
 let cliente1 = ["jose", "Colorado", "chevrolet", 312456778];
 cliente1.forEach(element => {
     console.log(element);
@@ -11,3 +16,14 @@ let cliente3 = ["Dubian", "Joy", "chevrolet", 3103937621];
 cliente3.forEach(element => {
     console.log(element);
 });
+
+function calcularDeudaconparametros(Pago, Precio) {
+    Pago = 20000000;
+    Precio = 40000290;
+    const deuda = Precio - Pago;
+    return deuda;
+}
+let Pago = 20000000;
+let Precio = 40000290;
+const deuda = calcularDeudaconparametros(Precio, Pago);
+console.log(`la deuda es de ${deuda}`);
