d8c3427922d8e5901ef45214f10c7c6472c2bca4
Dubian <dsamueLuriana@uniguajira.edu.co>
2021-10-05 20:03:48 -0500

    modificacion

---
+++ typescripe/app/app.ts
@@ -0,0 +1,129 @@
+//array
+
+let modelo: string[] = ["Beat","Onix turbo sedan", "joy","Joy sedan","onix turbo rs","onix turbo hb",
+"Colorado","camaro six ss",];
+console.log(modelo);
+
+let cantautos:number[] = [20, 7, 12, 5, 10, 18, 27, 15];
+console.log(cantautos);
+
+//Tuplas
+
+let cliente1:[string, string, string, number]= ["jose", "Colorado", "chevrolet",312456778]
+cliente1.forEach(element => {
+    console.log(element)
+});
+let cliente2:[string, string, string, number]= ["Sandra", "camaro six ss", "chevrolet",3134727890]
+cliente2.forEach(element => {
+    console.log(element)
+});
+
+let cliente3:[string, string, string, number]= ["Dubian", "Joy", "chevrolet",3103937621]
+cliente3.forEach(element => {
+    console.log(element)
+});
+
+//objetos
+
+interface clientes {
+    cedula:number
+    Nombre: String
+    Apellido: string
+    Telefono:number
+    Ciudad:string
+}
+const client1: clientes= {
+    cedula: 84562769,
+    Nombre: "Dubian",
+    Apellido:"Uriana",
+    Telefono: 3117478900, 
+    Ciudad:"Riohacha"
+}
+console.log(`${client1.Nombre}, ${client1.Apellido}, ${client1.cedula}, ${client1.Telefono},
+${client1.Ciudad}`)
+
+
+const client2: clientes= {
+    cedula: 40543678,
+    Nombre: "Jose",
+    Apellido:"Ocampo",
+    Telefono: 3234678942, 
+    Ciudad:"Riohacha"
+}
+console.log(`${client2.Nombre}, ${client2.Apellido}, ${client2.cedula}, ${client2.Telefono},
+${client2.Ciudad}`)
+
+
+const client3: clientes= {
+    cedula: 87543541,
+    Nombre: "sandra",
+    Apellido:"Daza",
+    Telefono: 3134678932, 
+    Ciudad:"Riohacha"
+}
+console.log(`${client3.Nombre}, ${client3.Apellido}, ${client3.cedula}, ${client3.Telefono},
+${client3.Ciudad}`)
+
+//funcion
+
+function calcularDeudaconparametros(Pago:number, Precio:number):number{
+    Pago=20000000;
+    Precio=40000290;
+  const deuda= Precio-Pago;
+  return deuda
+  }
+  
+  
+  let Pago:number=20000000;
+  let Precio:number=40000290;
+  const deuda= calcularDeudaconparametros(Precio,Pago)
+  console.log(`la deuda es de ${deuda}`);
+
+  // FUCIONES CON PARAMETROS
+
+  let mostrarMarca=function(nom: string){
+    console.log(`La Marca es ${nom}`)
+    }
+    
+    mostrarMarca("Chevrolet");
+    console.log(mostrarMarca);
+
+
+    let mostrarNcliente=function(nom: string){
+        console.log(`El nombre del cliente es ${nom}`)
+        }
+        
+        mostrarNcliente("Pedro Suarez");
+        console.log(mostrarNcliente);
+
+// FUNCIONES ANONIMAS
+
+let Calcularprecio=(preciobase:number, IVA:number=400000):number =>{
+    let s: number= 0;
+    if (IVA === undefined){
+        s = preciobase
+    } else{
+        s = preciobase+IVA
+    }
+    return s 
+};
+
+
+let sum:number = Calcularprecio(17804538,2000000); 
+console.log(sum);
+
+
+function Modeloprecio(){
+    return 2000000;
+}
+
+let mostrarModelo= function(Modelo: string, Precio: number=Modeloprecio()) {
+    console.info(`Modelo: ${Modelo} - Precio: ${Precio}`)
+
+    if (Precio == 2000000){
+
+        console.warn("No se encuentra dicho modelo")
+    } 
+}
+mostrarModelo("joy", 20000000);
+
---
+++ typescripe/js/app.js
@@ -0,0 +1,87 @@
+"use strict";
+let modelo = ["Beat", "Onix turbo sedan", "joy", "Joy sedan", "onix turbo rs", "onix turbo hb",
+    "Colorado", "camaro six ss",];
+console.log(modelo);
+let cantautos = [20, 7, 12, 5, 10, 18, 27, 15];
+console.log(cantautos);
+let cliente1 = ["jose", "Colorado", "chevrolet", 312456778];
+cliente1.forEach(element => {
+    console.log(element);
+});
+let cliente2 = ["Sandra", "camaro six ss", "chevrolet", 3134727890];
+cliente2.forEach(element => {
+    console.log(element);
+});
+let cliente3 = ["Dubian", "Joy", "chevrolet", 3103937621];
+cliente3.forEach(element => {
+    console.log(element);
+});
+const client1 = {
+    cedula: 84562769,
+    Nombre: "Dubian",
+    Apellido: "Uriana",
+    Telefono: 3117478900,
+    Ciudad: "Riohacha"
+};
+console.log(`${client1.Nombre}, ${client1.Apellido}, ${client1.cedula}, ${client1.Telefono},
+${client1.Ciudad}`);
+const client2 = {
+    cedula: 40543678,
+    Nombre: "Jose",
+    Apellido: "Ocampo",
+    Telefono: 3234678942,
+    Ciudad: "Riohacha"
+};
+console.log(`${client2.Nombre}, ${client2.Apellido}, ${client2.cedula}, ${client2.Telefono},
+${client2.Ciudad}`);
+const client3 = {
+    cedula: 87543541,
+    Nombre: "sandra",
+    Apellido: "Daza",
+    Telefono: 3134678932,
+    Ciudad: "Riohacha"
+};
+console.log(`${client3.Nombre}, ${client3.Apellido}, ${client3.cedula}, ${client3.Telefono},
+${client3.Ciudad}`);
+function calcularDeudaconparametros(Pago, Precio) {
+    Pago = 20000000;
+    Precio = 40000290;
+    const deuda = Precio - Pago;
+    return deuda;
+}
+let Pago = 20000000;
+let Precio = 40000290;
+const deuda = calcularDeudaconparametros(Precio, Pago);
+console.log(`la deuda es de ${deuda}`);
+let mostrarMarca = function (nom) {
+    console.log(`La Marca es ${nom}`);
+};
+mostrarMarca("Chevrolet");
+console.log(mostrarMarca);
+let mostrarNcliente = function (nom) {
+    console.log(`El nombre del cliente es ${nom}`);
+};
+mostrarNcliente("Pedro Suarez");
+console.log(mostrarNcliente);
+let Calcularprecio = (preciobase, IVA = 400000) => {
+    let s = 0;
+    if (IVA === undefined) {
+        s = preciobase;
+    }
+    else {
+        s = preciobase + IVA;
+    }
+    return s;
+};
+let sum = Calcularprecio(17804538, 2000000);
+console.log(sum);
+function Modeloprecio() {
+    return 2000000;
+}
+let mostrarModelo = function (Modelo, Precio = Modeloprecio()) {
+    console.info(`Modelo: ${Modelo} - Precio: ${Precio}`);
+    if (Precio == 2000000) {
+        console.warn("No se encuentra dicho modelo");
+    }
+};
+mostrarModelo("joy", 20000000);
